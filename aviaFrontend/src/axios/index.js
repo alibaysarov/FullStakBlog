6bbb5ee22c2bdcef250182bb2f71f6666325d4ce
alibaysarov <alibajsarov353@gmail.com>
2023-05-01 16:32:45 +0300

    Prod

--- aviaFrontend/src/axios/index.js
+++ aviaFrontend/src/axios/index.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-const instance=axios.create({
-  baseURL:'http://localhost:5000'
-})
-export default instance
+import axios from "axios";
+const instance = axios.create({
+  baseURL: "https://aviation-blog.vercel.app",
+});
+export default instance;
